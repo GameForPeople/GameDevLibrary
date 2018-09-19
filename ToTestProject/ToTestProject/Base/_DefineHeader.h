8434077c30a88dde1b7046677f6a1abcfad6e90a
GameForPeople <koreagamemaker@gmail.com>
2018-09-20 00:32:42 +0900

    add Define Header

---
+++ ToTestProject/ToTestProject/Base/_DefineHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define				INLINE			__inline
+#define				PI				3.141592f
+
+#include <iostream>	
+
+// BaseRandomEngine
+#include <random>
+#include <ctime>
+
+// vector 3D
+#include <cmath>
+#include <string>
